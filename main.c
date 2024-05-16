495ee500e8e59fd6bd84a26368476a609fcb6652
RANA <hanna.orl.melo@gmail.com>
2024-05-16 19:49:27 -0300

    colocando a opção 1
    implementando as funcao criar cliente

--- main.c
+++ main.c
@@ -3,7 +3,8 @@
 
 
 int main(){
-    // Inicializa as listas e executa o loop principal.
+
+    
     while (1) {
         printf("1: Novo cliente\n");
         printf("2: Apaga cliente\n");
@@ -15,6 +16,39 @@ int main(){
         printf("8: Sair\n");
         printf("9: Relistar as opcoes\n");
 
+        int op;
+        printf("Digite a opção: ");
+        scanf("%d", &op);
 
+        switch (op) {
+            case 1:
+                NovoCliente();
+                break;
+            case 2:
+                ApagaCliente();
+                break;
+            case 3:
+                ListarClientes();
+                break;
+            case 4:
+                Debito();
+                break;
+            case 5:
+                Deposito();
+                break;
+            case 6:
+                Extrato();
+                break;
+            case 7:
+                TransferenciaEntreContas();
+                break;
+            case 8:
+                printf("Você saiu. Volte sempre!\n");
+                return 0;
+            default:
+                printf("Opção inválida\n");
+        }
     }
+
+
 }
