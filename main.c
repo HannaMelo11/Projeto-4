f9cfa5b4e9adba7347f04d907752a0b2b6629325
RANA <hanna.orl.melo@gmail.com>
2024-05-16 15:53:12 -0300

    Criando a lista de funções
    criando a lista de funções, mas ainda sem implementação

---
+++ main.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "biblioteca.h"
+
+
+int main(){
+    // Inicializa as listas e executa o loop principal.
+    while (1) {
+        printf("1: Novo cliente\n");
+        printf("2: Apaga cliente\n");
+        printf("3: Listar clientes\n");
+        printf("4: Debito\n");
+        printf("5: Deposito\n");
+        printf("6: Extrato\n");
+        printf("7: Transferencia entre contas\n");
+        printf("8: Sair\n");
+        printf("9: Relistar as opcoes\n");
+
+
+    }
+}
