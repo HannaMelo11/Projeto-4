3185ace6e0005484551d1a73cd83ec077e93bec1
HannaMelo11 <93196404+HannaMelo11@users.noreply.github.com>
2024-05-16 21:18:59 -0300

    colocando a correçao do bug na main
    colocando a correção de bug na main

---
+++ cmake-build-debug/CMakeFiles/Projeto4.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Projeto4.dir/biblioteca.c.obj"
+  "CMakeFiles/Projeto4.dir/biblioteca.c.obj.d"
+  "CMakeFiles/Projeto4.dir/main.c.obj"
+  "CMakeFiles/Projeto4.dir/main.c.obj.d"
+  "Projeto4.exe"
+  "Projeto4.exe.manifest"
+  "Projeto4.pdb"
+  "libProjeto4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Projeto4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
